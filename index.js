830967937b0156ec3412e2f4a22d43f8e14f8710
BentlyM <metayerbently@gmail.com>
2024-06-20 17:46:13 -0400

    addded some display for content

--- index.js
+++ index.js
@@ -18,16 +18,20 @@ function Book(title , author, pages, read) {
     }
 }
 
-createBook.addEventListener('click', ()=>{
+createBook.addEventListener('click', (event)=>{
+    event.preventDefault();
     const title = document.getElementById('book-name').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
+    console.log(myLibrary);
     if(title === '' || author === '' || pages === '') throw `the field is empty`;
     const newBook = new Book(title, author, pages);
     myLibrary.push(newBook);
     renderCard(newBook);
     viewDrop(); // toggles
 })
+
+
 
 function renderCard(renderBook){
     if(container.children.length >= 30 || 30 <= myLibrary.length) throw `MAX_LENGTH REACHED`;
@@ -41,12 +45,28 @@ function renderCard(renderBook){
 
     const cardContent = document.createElement('div');
     cardContent.className = 'card-content';
+    displayContent(renderBook.title, renderBook.author, renderBook.pages, cardContent);
 
     const editCard = document.createElement('div');
     editCard.className = 'mutate-card';
+    //editCardFunction(renderBook);
 
     card.appendChild(NumCard);
     card.appendChild(cardContent);
     card.appendChild(editCard);
     container.appendChild(card);
+}
+
+const displayContent = (title, author, pages, cardContent) => { 
+    const displayTitle = document.createElement('span');
+    const displayAuthor = document.createElement('span');
+    const displayPages = document.createElement('span');
+
+    displayTitle.textContent = `Title: ${title}`;
+    displayAuthor.textContent = `Author: ${author}`;
+    displayPages.textContent = `Pages: ${pages}`;
+
+    cardContent.appendChild(displayTitle);
+    cardContent.appendChild(displayAuthor);
+    cardContent.appendChild(displayPages);
 }
